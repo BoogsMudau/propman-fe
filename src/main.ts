53ba6206d827c963b422169ebeb8ab39777c8c80
BoogsMudau <vhugala.m00@gmail.com>
2025-10-12 16:28:12 +0200

    comments send

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { addIcons } from 'ionicons';
-import { home, notifications, person, build, add, chatbubbleOutline } from 'ionicons/icons';
+import {
+  home,
+  notifications,
+  person,
+  build,
+  add,
+  chatbubbleOutline,
+  sendOutline,
+} from 'ionicons/icons';
 
 addIcons({
   home,
@@ -11,5 +19,6 @@ addIcons({
   build,
   add,
   chatbubbleOutline,
+  sendOutline,
 });
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
